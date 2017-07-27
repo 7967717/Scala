9cc93f294b535ec527cd1b3d905c86aede912f3a
Roman Rudenko <7967717@gmail.com>
2017-07-27 22:23:17 +0300

    Equilibrium

---
+++ scala0/src/main/java/io/rr/scala/core/start/examples/Equilibrium.scala
@@ -0,0 +1,32 @@
+package io.rr.scala.core.start.examples
+
+/**
+  * @author roman on 7/27/17.
+  */
+object Equilibrium {
+  def main(args: Array[String]): Unit = {
+    def solution(a: Array[Int]): Int = {
+      // write your code in Scala 2.12
+      def diffAbs(a: Int, b: Int): Int = if (a - b < 0) b - a else a - b
+
+      def findDiff(list: List[Int], leftSum: Int, rightSum: Int, diff: Int): Int = {
+        list match {
+          case x1 :: x2 :: xs =>
+            val curDiff = diffAbs(leftSum, rightSum)
+            val bestDiff = if (curDiff < diff) curDiff else diff
+            findDiff(list.tail, leftSum + x1, rightSum - x1, bestDiff)
+          case _ => diff
+        }
+      }
+
+      val leftSum: Int = a(0)
+      val rightSum: Int = a.sum - a(0)
+      val diff = diffAbs(leftSum, rightSum)
+      findDiff(a.toList.tail, leftSum, rightSum, diff)
+    }
+
+    val arr = Array[Int](-1, 3, -4, 5, 1, -6, 2, 1)
+    println(solution(arr))
+
+  }
+}
