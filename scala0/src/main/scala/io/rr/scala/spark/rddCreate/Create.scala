f840814b6ad236de8af6560a4c524b8ba5e30c08
Roman Rudenko <7967717@gmail.com>
2017-10-16 18:36:49 +0300

    RatingsCounter

--- scala0/src/main/scala/io/rr/scala/spark/rddCreate/Create.scala
+++ scala0/src/main/scala/io/rr/scala/spark/rddCreate/Create.scala
@@ -8,16 +8,16 @@ import org.apache.spark.{SparkConf, SparkContext}
 object Create {
   def main(args: Array[String]): Unit = {
 
-//    val conf = new SparkConf().setAppName("Create RDD").setMaster("local[*]")
-//    val sc = new SparkContext(conf)
+    val conf = new SparkConf().setAppName("Create RDD").setMaster("local[*]")
+    val sc = new SparkContext(conf)
 
     val list = List("Winnie the Pooh", "The Tigger Movie", "Pirates of the Caribbean",
       "Apollo 13", "Mall Cop", "Mockingjay - Part 1", "The Good Dinosaur", "Lava",
       "The Peanuts Movie")
 
-//    ﻿sc.parallelize(list)
-//
-//    ﻿sc.parallelize(list).take(3)
+    sc.parallelize(list)
+
+    sc.parallelize(list).take(3)
 
   }
 
