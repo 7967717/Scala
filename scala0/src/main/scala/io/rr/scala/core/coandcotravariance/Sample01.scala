2fcff95014eeee7447b21e0fcf36df1d7dcff37d
roman <roman.rudenko@qvantel.com>
2018-11-03 14:26:03 +0200

    variance

--- scala0/src/main/scala/io/rr/scala/core/coandcotravariance/Sample01.scala
+++ scala0/src/main/scala/io/rr/scala/core/coandcotravariance/Sample01.scala
@@ -2,31 +2,53 @@ package io.rr.scala.core.coandcotravariance
 
 class Sample01 {
 
-  case class InvariantContainer[A]()
-  case class CovariantContainer[+A]()
-  case class ContravariantContainer[-A]()
+  sealed trait Container[A] {
+    def accessor: A
+    def mutator(a: A): Unit
+  }
+
+  case class InvariantContainer[A](var a: A) {
+      def accessor(): A = a
+      def mutator(a0: A): Unit = {
+        a = a0
+      }
+}
+//  CovariantContainer
+  case class Producer[+A](a: A) {
+    def produce(): A = a
+//    def mutator(a0: A): Unit = {
+//      a = a0
+//    }
+  }
+//  ContravariantContainer
+  case class Consumer[-A](var a: A) {
+//    def accessor():A = a
+    def consume(a0: A): Unit = {
+      a = a0
+    }
+  }
 
   class Car
   class Subaru extends Car
 
   // works
-  val inv1: InvariantContainer[Subaru] = InvariantContainer[Subaru]
-  val inv2: InvariantContainer[Car] = InvariantContainer[Car]
+  val inv: InvariantContainer[Subaru] = InvariantContainer[Subaru](new Subaru())
+  private val subaru: Subaru = inv.accessor()
+  inv.mutator(new Subaru())
   // doesn't work
 //  val inv3: InvariantContainer[Subaru] = InvariantContainer[Car]
 //  val inv3: InvariantContainer[Car] = InvariantContainer[Subaru]
 
   // works
-  val cov1: CovariantContainer[Subaru] = CovariantContainer[Subaru]
-  val cov2: CovariantContainer[Car] = CovariantContainer[Car]
-  val cov3: CovariantContainer[Car] = CovariantContainer[Subaru]
+  val cov: Producer[Car] = Producer[Car](new Subaru())
+  private val car1: Car = cov.produce()
   // doesn't work
 //  val inv4: InvariantContainer[Subaru] = InvariantContainer[Car]
 
   // works
-  val con1: ContravariantContainer[Subaru] = ContravariantContainer[Subaru]
-  val con2: ContravariantContainer[Subaru] = ContravariantContainer[Car]
-  val con3: ContravariantContainer[Subaru] = ContravariantContainer[Car]
+  val con: Consumer[Car] = Consumer[Car](new Car())
+  con.consume(new Car())
+  con.consume(new Subaru())
   // doesn't work
 //  val con4: ContravariantContainer[Car] = ContravariantContainer[Subaru]
 
